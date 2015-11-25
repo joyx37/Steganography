ed58e467290d92cc8e8604c0e4f072b199f05279
joyx37 <ajoy3792@gmail.com>
2015-11-25 15:38:06 -0500

    Camera Intent Added

--- app/src/main/java/com/example/ajoy3/steganography/TextInImage.java
+++ app/src/main/java/com/example/ajoy3/steganography/TextInImage.java
@@ -1,6 +1,10 @@
  package com.example.ajoy3.steganography;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,10 +12,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class TextInImage extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
 
-    @Override
+ public class TextInImage extends AppCompatActivity {
+
+     private static final int REQUEST_CAMERA = 1;
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_in_image);
@@ -22,8 +33,8 @@ public class TextInImage extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_CAMERA);
             }
         });
     }
@@ -49,4 +60,37 @@ public class TextInImage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+         // TODO Auto-generated method stub
+         super.onActivityResult(requestCode, resultCode, data);
+         if (requestCode == REQUEST_CAMERA) {
+             Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+             String imageName = Long.toString(new Date().getTime()) + ".jpg";
+             String state = Environment.getExternalStorageState();
+             File file;
+             if (Environment.MEDIA_MOUNTED.equals(state)) {
+                 File directory = new File(Environment.getExternalStorageDirectory()+"/Steganos/Images/Sent/");
+                 directory.mkdirs();
+                 file = new File(directory, imageName);
+                 try {
+                     file.createNewFile();
+                     FileOutputStream out = new FileOutputStream(file);
+                     bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                     out.flush();
+                     out.close();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }
+         }
+     }
+
+
+     public void SettingOnClick(MenuItem item) {
+
+    }
+
+     public void PairDevicesOnClick(MenuItem item) {
+     }
+ }
